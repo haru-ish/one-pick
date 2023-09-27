c1fca15e43d078d2e5d03ad83f9c2678cbec908d
haru-ish <haru-ish@j0d.de>
2023-09-27 19:21:51 +0200

    Fix a class: MovieViewModel
    - クラス宣言に、chatGptRepository型のprivateコンストラクタパラメータchatGptRepositoryを追加
    - 直接Repositoryクラスを呼び出していた変数chatGptRepositoryを削除
    - Factoryオブジェクトを作成し、アプリケーションコンテナを使用してchatGptRepositoryを取得する実装に変更

--- app/src/main/java/com/example/onepick/ui/screens/MovieViewModel.kt
+++ app/src/main/java/com/example/onepick/ui/screens/MovieViewModel.kt
@@ -1,13 +1,19 @@
 package com.example.onepick.ui.screens
 
+import android.text.Spannable.Factory
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
 import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.onepick.ChatGptApplication
 import com.example.onepick.data.ChatGptRepository
 import kotlinx.coroutines.launch
 import retrofit2.HttpException
 import java.io.IOException
 
-class MovieViewModel : ViewModel() {
+class MovieViewModel(private val chatGptRepository: ChatGptRepository) : ViewModel() {
     /**
      * ChatGptApi Retrofitサービスから映画名を取得
      */
@@ -15,7 +21,6 @@ class MovieViewModel : ViewModel() {
         // coroutineを起動
         viewModelScope.launch {
             try {
-                val chatGptRepository = ChatGptRepository.NetworkChatGPTRepository()
                 val result = chatGptRepository.getRecommendedMovie(keyword1, keyword2, keyword3)
             } catch (e: IOException) {
 
@@ -23,9 +28,16 @@ class MovieViewModel : ViewModel() {
 
             }
         }
-
-
-
     }
-
+    // アプリケーションコンテナによってRepositoryをViewModelに提供することで、ViewModelがRepositoryを作成するのを回避
+    companion object {
+        // Factoryオブジェクトはアプリケーションコンテナを使用して、chatGptRepositoryを取得
+        val Factory: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                val application = (this[APPLICATION_KEY] as ChatGptApplication)
+                val chatGptRepository = application.container.chatGptRepository
+                MovieViewModel(chatGptRepository = chatGptRepository)
+            }
+        }
+    }
 }
