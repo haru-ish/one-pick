12a1c8db411b200d056b8531dc9871c1209262a8
haru-ish <haru-ish@j0d.de>
2023-09-29 11:39:13 +0200

    fix a class: MainActivity
    - セットアップ時に自動生成された不必要なクラスをコメントアウト

--- app/src/main/java/com/example/onepick/MainActivity.kt
+++ app/src/main/java/com/example/onepick/MainActivity.kt
@@ -6,10 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.onepick.ui.OnePickApp
 import com.example.onepick.ui.theme.OnePickTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +20,25 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    OnePickApp()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    OnePickTheme {
-        Greeting("Android")
-    }
-}
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    OnePickTheme {
+//        Greeting("Android")
+//    }
+//}
