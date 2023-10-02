13a257919324499298387104d62157886c241475
haru-ish <haru-ish@j0d.de>
2023-10-02 19:27:45 +0200

    Fix a class: ChatGptRepository
    - 戻り値をMovieからChatGptResponseクラスに変更

--- app/src/main/java/com/example/onepick/data/ChatGptRepository.kt
+++ app/src/main/java/com/example/onepick/data/ChatGptRepository.kt
@@ -1,14 +1,12 @@
 package com.example.onepick.data
 
-import com.example.onepick.model.Movie
 import com.example.onepick.network.ChatGptApiService
-
 
 /**
  * chatGptApiから映画のタイトルを取得するリポジトリ
  */
 interface ChatGptRepository {
-    suspend fun getRecommendedMovie(keyword1: String, keyword2: String, keyword3: String): Movie
+    suspend fun getRecommendedMovie(request: ChatGptRequest): ChatGptResponse
 
     /**
      * chatGPTApiから映画名を取得するリポジトリのネットワーク実装
@@ -16,8 +14,8 @@ interface ChatGptRepository {
     class NetworkChatGPTRepository(
         private val chatGptApiService: ChatGptApiService
     ) : ChatGptRepository {
-        override suspend fun getRecommendedMovie(keyword1: String, keyword2: String, keyword3: String): Movie =
-            chatGptApiService.getRecommendedMovie(keyword1, keyword2, keyword3)
+        override suspend fun getRecommendedMovie(request: ChatGptRequest): ChatGptResponse =
+            chatGptApiService.getRecommendedMovie(request)
     }
 }
 
