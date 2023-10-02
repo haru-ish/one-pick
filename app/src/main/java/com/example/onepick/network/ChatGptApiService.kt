897e2b5e7201bc952d92216b7c2ba2721207327f
haru-ish <haru-ish@j0d.de>
2023-10-02 19:39:28 +0200

    Fix a class: ChatGptApiService
    - getRecommendedMovie()の戻り値をMovieからChatGptResponseクラスに変更
    - 引数をStringからChatGptRequestクラスに変更
    - @Headers, @POSTアノテーションを追加し、API通信に必要な情報を記載

--- app/src/main/java/com/example/onepick/network/ChatGptApiService.kt
+++ app/src/main/java/com/example/onepick/network/ChatGptApiService.kt
@@ -1,18 +1,21 @@
 package com.example.onepick.network
 
-import com.example.onepick.model.Movie
-import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
-import kotlinx.serialization.json.Json
-import okhttp3.MediaType.Companion.toMediaType
-import retrofit2.Retrofit
-import retrofit2.http.GET
-import retrofit2.http.Query
 import com.example.onepick.BuildConfig
+import com.example.onepick.data.ChatGptRequest
+import com.example.onepick.data.ChatGptResponse
+import retrofit2.http.Body
+import retrofit2.http.Headers
+import retrofit2.http.POST
 
 /**
  * APIコールを作成するためのRetrofitサービスオブジェクト
  */
 interface ChatGptApiService {
-    @GET("recommend")
-    suspend fun getRecommendedMovie(@Query("keyword1") keyword1: String, @Query("keyword2") keyword2: String, @Query("keyword3") keyword3: String): Movie
+    @Headers(
+        "Content-Type: application/json",
+        "Authorization: Bearer ${BuildConfig.CHATGPT_API_KEY}"
+    )
+    @POST("v1/chat/completions")
+    suspend fun getRecommendedMovie(@Body request: ChatGptRequest): ChatGptResponse
 }
+
