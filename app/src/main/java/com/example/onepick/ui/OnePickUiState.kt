73eabbada0e532f3d55c4bcf196d9a7dd9d8d384
haru-ish <haru-ish@j0d.de>
2023-10-06 15:09:03 +0200

    Fix a class: OnePickUiState
    - LoadingNextApiを追加

--- app/src/main/java/com/example/onepick/ui/OnePickUiState.kt
+++ app/src/main/java/com/example/onepick/ui/OnePickUiState.kt
@@ -4,5 +4,6 @@ sealed interface OnePickUiState {
     data class Success(val content: String) : OnePickUiState
     data class Error(val msg: String) : OnePickUiState
     object Loading : OnePickUiState
+    object LoadingNextApi : OnePickUiState
     object Initial : OnePickUiState
 }
