28b63a6fb374760fa591596c56f355e1e0e41992
haru-ish <haru-ish@j0d.de>
2023-10-20 21:01:04 +0200

    Added new class: Genre.kt
    - 映画のジャンルを表示するためのデータクラスを追加

---
+++ app/src/main/java/com/example/onepick/model/Genre.kt
@@ -0,0 +1,28 @@
+package com.example.onepick.model
+
+/**
+ * 　映画のジャンルを表すデータクラス
+ */
+data class Genre(val id: Int, val name: String)
+
+val genres = listOf(
+    Genre(28, "アクション"),
+    Genre(12, "アドベンチャー"),
+    Genre(16, "アニメ"),
+    Genre(35, "コメディ"),
+    Genre(80, "クライム"),
+    Genre(99, "ドキュメンタリー"),
+    Genre(18, "ドラマ"),
+    Genre(10751, "ファミリー"),
+    Genre(14, "ファンタジー"),
+    Genre(36, "歴史"),
+    Genre(27, "ホラー"),
+    Genre(10402, "音楽"),
+    Genre(9648, "ミステリー"),
+    Genre(10749, "恋愛"),
+    Genre(878, "SF"),
+    Genre(10770, "テレビ映画"),
+    Genre(53, "スリラー"),
+    Genre(10752, "戦争"),
+    Genre(37, "西部劇"),
+)
