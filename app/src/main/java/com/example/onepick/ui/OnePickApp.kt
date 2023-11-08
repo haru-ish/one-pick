10e15fa9dcff4fab1436782df4adcdc8cb96b418
haru-ish <haru-ish@j0d.de>
2023-11-08 17:40:36 +0900

    Fix a class: OnePickApp
    - アプリにバーとロゴを追加

--- app/src/main/java/com/example/onepick/ui/OnePickApp.kt
+++ app/src/main/java/com/example/onepick/ui/OnePickApp.kt
@@ -1,29 +1,78 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
 package com.example.onepick.ui
 
-import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.onepick.ui.screens.MovieSearchScreen
+import com.example.onepick.R
+import com.example.onepick.ui.screens.HomeScreen
 import com.example.onepick.ui.screens.OnePickViewModel
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun OnePickApp() {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
     Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = { OnePickTopAppBar(scrollBehavior = scrollBehavior) }
     ) {
         Surface(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(it)
         ) {
+            // OnePickViewModelのインスタンスを作成および初期化
             val onePickViewModel: OnePickViewModel = viewModel(factory = OnePickViewModel.Factory)
-            MovieSearchScreen(onePickViewModel = onePickViewModel)
+            HomeScreen(onePickViewModel = onePickViewModel)
         }
     }
+}
+
+@Composable
+fun OnePickTopAppBar(
+    scrollBehavior: TopAppBarScrollBehavior,
+    modifier: Modifier = Modifier
+) {
+    CenterAlignedTopAppBar(
+        scrollBehavior = scrollBehavior,
+        colors = TopAppBarDefaults.smallTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.primary,
+        ),
+        title = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.dog_paw),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(dimensionResource(id = R.dimen.padding_extraLarge))
+                        .padding(dimensionResource(id = R.dimen.padding_small))
+                )
+                Text(
+                    text = stringResource(R.string.app_name),
+                    style = MaterialTheme.typography.headlineSmall,
+                )
+            }
+        },
+        modifier = modifier
+    )
 }
