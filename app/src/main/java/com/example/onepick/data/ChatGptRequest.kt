8f411f6227ed3c02baf253bfcbe2c68c75e4c052
haru-ish <haru-ish@j0d.de>
2023-11-21 14:54:33 +0900

    Delete some classes: ChatGptContainer.kt etc
    - webserverを介してOpenAI APIと通信する際に不要な以下のクラスを削除
    - ChatGptContainer.kt, ChatGptRepository.kt, ChatGptRequest.kt, ChatGptResponse.kt
    - Choice.kt, Message.kt, Usage.kt, ChatGptApiService.kt

--- app/src/main/java/com/example/onepick/data/ChatGptRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.example.onepick.data
-
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
-@Serializable
-data class ChatGptRequest(
-    @SerialName("model") val model: String,
-    @SerialName("messages") val messages: List<Message>
-)
